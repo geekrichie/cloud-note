4e6ec6f99b5de0c8110d6437065731cb4a16f3bf
geekrichie <h4481695abcd@163.com>
2020-07-08 18:29:52 +0800

    新增侧边栏

--- note-cloud/src/components/Main.vue
+++ note-cloud/src/components/Main.vue
@@ -1,16 +1,25 @@
 <template>
-    <div class="main">我是主页</div>
+    <div class="main">
+        <sidebar ></sidebar>
+    </div>
     
 </template>
 <script>
+import sidebar from './sidebar'
 export default {
-    created() {
-        
-    }
+   components :{
+       sidebar
+   }
 }
 </script>
 <style scoped>
 .main {
     text-align:left;
+    position: absolute;
+    top: 50px;
+    left: 0;
+    right: 0;
+    bottom: 0px;
 }
+
 </style>
---
+++ note-cloud/src/components/sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="sidebar">
+         <el-menu
+            class="el-menu-vertical-demo"        
+            background-color="#545c64"
+            text-color="#fff">
+            <el-menu-item index="1">
+                <div class="el-icon-plus"></div>
+                <span slot="title">新增笔记</span>
+            </el-menu-item> 
+        </el-menu>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.sidebar {
+    width:300px;
+    height:100%;
+}
+.el-icon-plus {
+    background-image:url("../assets/plus.png")
+}
+ul {
+    height:100%;
+}
+</style>
